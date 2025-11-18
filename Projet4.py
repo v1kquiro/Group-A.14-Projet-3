a071ed82d7920e4b9ce87d88bde95b75aeaa5475
Victor Quiroz Ibanez Spindler <quirozibanezvictor@gmail.com>
2025-11-18 17:05:38 +0100

    Update Projet4.py

--- Projet4.py
+++ Projet4.py
@@ -11,3 +11,11 @@ while True:
     message = radio.receive()
     if message:
         display.scroll(message)
+
+    if button_a.is_pressed() and button_b.is_pressed():
+        display.scroll('A and B')
+    elif button_a.is_pressed():
+        display.scroll('A')
+    elif button_b.is_pressed():
+        display.scroll('B')
+    sleep(100)
