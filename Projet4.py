f597b37666196388443ad7a3f08418fd16826bc9
Victor Quiroz Ibanez Spindler <quirozibanezvictor@gmail.com>
2025-11-18 17:20:52 +0100

    Update Projet4.py

--- Projet4.py
+++ Projet4.py
@@ -1,23 +1,43 @@
 from microbit import *
 import radio
+import log
 
-radio.config(group=23)
+radio.config(group=32) #l'alimentation pour recevoir les msgs etc pour microbit
 
 radio.on()
-radio.send('hello')
-radio.config(group=23, power=3)
+radio.send('message')
+radio.config(group=32, power=6) #avoir access a recevoir les msgs
+
+count = 0
+score = 0
+score += 1
+display.scroll(score)
+
+name = 'Parent'
+display.scroll(name) #nommee la microbit
 
 log.set_labels('temperature','sound','light')
 log.add({
     'temperature' : temperature(),
     'sound' : microphone.sound_level(),
     'light' : display.read_light_level()
-})
+}) #crees les valeurs pour temp son et lumiere
 
+@run_every(s=30)
+def log_data():
+    log.add({
+    'temperature' : temperature(),
+    'sound' : microphone.sound_level(),
+    'light' : display.read_light_level()
+    })
+    #log les entrees chaque 30 sec
+
+running = False
 while True:
     message = radio.receive()
     if message:
         display.scroll(message)
+        #recevoir les msgs
 
     if button_a.is_pressed() and button_b.is_pressed():
         display.scroll('A and B')
@@ -26,4 +46,14 @@ while True:
     elif button_b.is_pressed():
         display.scroll('B')
     sleep(100)
+    #si les boutons A et B sont appuyer
 
+    sleep(10000)
+
+    if button_a.was_pressed():
+        running = not running
+    if running:
+        display.show(1)
+    else:
+        display.show(0)
+    #sauvegrader du data
