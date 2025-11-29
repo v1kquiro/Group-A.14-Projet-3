95b8cc8c3eb18f12c47184f575c5f3e36b16a827
EMLB18 <100311313+EMLB18@users.noreply.github.com>
2025-11-30 00:46:16 +0100

    Update squelette_bebi_sécu.py

--- squelette_bebi_sécu.py
+++ squelette_bebi_sécu.py
@@ -83,8 +83,24 @@ def send_packet(key, type, content):
            (str) content:   Données à envoyer
 	:return none
     """
+     # construction du format  TYPE|LONGUEUR|CONTENU/VALEUR
+ 
+    #T: type du message
+    T = type
+    #L : Longeur du message
+    L= str(len(content))
+    #V = content or value
+    V= content
+ 
+    # Assemblage du Format TLV
+    packet_TLV = T + "|" + L + "|" + V       # "|" : separateur
+ 
+    #chiffrement du packet
+    encrypted_packet = vigenere(packet_TLV, key, decryption=False)
+    #envoi via radio
+    radio.send(encrypted_packet)
+    
 
-#Decrypt and unpack the packet received and return the fields value
 def unpack_data(encrypted_packet, key):
     """
     Déballe et déchiffre les paquets reçus via l'interface radio du micro:bit
@@ -97,6 +113,23 @@ def unpack_data(encrypted_packet, key):
             (str) message:         Données reçues
     """
 
+            #Déchiffrementdu packet
+    decrypted_packet = vigenere(encrypted_packet, key, decryption =True)
+ 
+                     #Extraction du Format TLV
+        #séparer selon le délimitieur "|"
+    parts = decrypted_packet.split("|")
+ 
+        #verifie que l'on a bien le 3 parties (T, L, V)
+    if len(parts) == 3 :
+        T = parts[0]                     # Le type
+        L = int(parts[1])                # La longueur( converti en e
+        V = parts[2]                     # La valeur (donnée)
+        return T, L, V
+ 
+      #continuer le code encore manque donnée
+    else:
+        return "", 0, ""   # renvoi paquet vide en cas d'erreur 
 
 #Unpack the packet, check the validity and return the type, length and content
 def receive_packet(packet_received, key):
@@ -111,7 +144,13 @@ def receive_packet(packet_received, key):
             (int)lenght:           Longueur de la donnée en caractère
             (str) message:         Données reçue
     """
-    
+    try:
+        T, L, V = unpack_data(packet_received, key)
+        return T, L, V
+ 
+    except: 
+        return "",0, ""
+
 #Calculate the challenge response
 def calculate_challenge_response(challenge):
     """
@@ -120,6 +159,8 @@ def calculate_challenge_response(challenge):
     :param (str) challenge:            Challenge reçu
 	:return (srt)challenge_response:   Réponse au challenge
     """
+
+
 #Ask for a new connection with a micro:bit of the same group
 def establish_connexion(key):
     """
@@ -129,7 +170,7 @@ def establish_connexion(key):
     :param (str) key:                  Clé de chiffrement
 	:return (srt)challenge_response:   Réponse au challenge
     """
-    # generer un nombre aleatoire pour aleatoire( 8 chiffres )
+    # Generer un nombre aleatoire pour aleatoire( 8 chiffres )
     nonce = str(random.randint(10000000, 99999999))  # Générer un nonce aleatoire
     display.scroll("N:" + nonce[:4]) # vas afficher les 4 premieers chiffres
 
@@ -167,10 +208,20 @@ def establish_connexion(key):
                         display.show(Image.No) #hash incorrect
                         return False
         sleep(100)
-
-
-
     
 
-def main():
-    return True
+def main():     # ici je retiens que le gosse est actif 
+    global connexion_established
+
+    while True:
+        if not connexion_established:
+              #ici il est actif il cherche a se connecter au parent
+            if button_a.was_pressed():
+                result = establish_connexion(key)
+                if result :
+                    connexion_established = True
+        else:
+              # il envoi un status
+             if button_a.was_pressed():
+                  send_packet(key, "STATUS", "CRYING")
+            
